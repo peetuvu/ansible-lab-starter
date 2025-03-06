121ee4107b1ea9ad940d41e6fb55c1ef9fc08d73
donpedro <peetuvuorela@gmail.com>
2025-03-06 09:45:51 +0000

    Add test scripts to test CI workflow

---
+++ scripts/pythontest.py
@@ -0,0 +1,10 @@
+print("Testing python script...")
+
+def test(a, b):
+    return a + b
+
+if test(2, 3) == 5:
+    print("WOOHOO")
+
+else:
+    print("oh no")
---
+++ scripts/setup.sh
@@ -0,0 +1,3 @@
+echo "Running shell script test..."
+# Do something minimal—maybe list files or show hostname
+hostname
